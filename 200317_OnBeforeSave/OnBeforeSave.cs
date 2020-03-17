5d3badc363f3eeca00a19f76c22ab34c7811a953
AZ <zhengheng36@gmail.com>
2020-03-17 12:52:36 -0400

    - Add .sln - Add OnBeforeSave, VSEventHandler

---
+++ 200317_OnBeforeSave/OnBeforeSave.cs
@@ -0,0 +1,34 @@
+﻿namespace _200317_OnBeforeSave
+{
+	using System;
+	using System.Runtime.InteropServices;
+	using Microsoft.VisualStudio.Shell;
+
+	/// <summary>
+	/// This class implements the tool window exposed by this package and hosts a user control.
+	/// </summary>
+	/// <remarks>
+	/// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane,
+	/// usually implemented by the package implementer.
+	/// <para>
+	/// This class derives from the ToolWindowPane class provided from the MPF in order to use its
+	/// implementation of the IVsUIElementPane interface.
+	/// </para>
+	/// </remarks>
+	[Guid("e0a2565d-62c1-459e-b8a9-f96e9e7a14e0")]
+	public class OnBeforeSave : ToolWindowPane
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="OnBeforeSave"/> class.
+		/// </summary>
+		public OnBeforeSave() : base(null)
+		{
+			this.Caption = "OnBeforeSave";
+
+			// This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
+			// we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
+			// the object returned by the Content property.
+			this.Content = new OnBeforeSaveControl();
+		}
+	}
+}
---
+++ 200317_OnBeforeSave/_200317_OnBeforeSavePackage.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.Shell;
+using Task = System.Threading.Tasks.Task;
+
+namespace _200317_OnBeforeSave
+{
+    /// <summary>
+    /// This is the class that implements the package exposed by this assembly.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The minimum requirement for a class to be considered a valid package for Visual Studio
+    /// is to implement the IVsPackage interface and register itself with the shell.
+    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+    /// to do it: it derives from the Package class that provides the implementation of the
+    /// IVsPackage interface and uses the registration attributes defined in the framework to
+    /// register itself and its components with the shell. These attributes tell the pkgdef creation
+    /// utility what data to put into .pkgdef file.
+    /// </para>
+    /// <para>
+    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
+    /// </para>
+    /// </remarks>
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [Guid(_200317_OnBeforeSavePackage.PackageGuidString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [ProvideToolWindow(typeof(OnBeforeSave))]
+    public sealed class _200317_OnBeforeSavePackage : AsyncPackage
+    {
+        /// <summary>
+        /// _200317_OnBeforeSavePackage GUID string.
+        /// </summary>
+        public const string PackageGuidString = "4110c5d5-d531-4254-8491-a969cba58378";
+
+        #region Package Members
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        /// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
+        /// <param name="progress">A provider for progress updates.</param>
+        /// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            // When initialized asynchronously, the current thread may be a background thread at this point.
+            // Do any initialization that requires the UI thread after switching to the UI thread.
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+			RunningDocTableEvents testRTE = new RunningDocTableEvents(this);
+			testRTE.BeforeSave += onBeforeSave;
+
+            await OnBeforeSaveCommand.InitializeAsync(this);
+		}
+
+		private void onBeforeSave(object sender, EnvDTE.Document document)
+		{
+			MessageBox.Show("Before Save TEST GOOD!");
+		}
+
+		#endregion
+	}
+}
