42768891f2a01b74a0119a078f369ac5df69e968
Aaron Zheng Visual Studio <zhengheng36@gmail.com>
2020-10-24 13:12:15 -0400

    clean up home pc

---
+++ TEST/AZClass1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$
+using System.Text;
+
+namespace $safeprojectname$
+{
+    public class AZClass1
+{
+
+}
+}
---
+++ TEST/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Text;
+
+namespace TEST
+{
+    public class Class1
+    {
+        
+    }
+}
